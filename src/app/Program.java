858e3cec3fcb2ad68d33758461b68f294fe63beb
JeanButtei <jeanbuttei@gmail.com>
2020-12-16 13:11:46 -0300

    Department entity class

---
+++ src/app/Program.java
@@ -0,0 +1,14 @@
+package app;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department dept = new Department(1, "Books");
+		System.out.println(dept);
+
+	}
+
+}
